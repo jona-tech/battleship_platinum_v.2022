42faa8f0e36022cf6b8fd53fcb5d1af823838cde
jona-tech <jonathanbernhardsson1@gmail.com>
2022-12-12 12:14:45 +0000

    add function to place 5 ships in random locations

--- run.py
+++ run.py
@@ -10,12 +10,25 @@ VISIBLE_BOARD = [[' '] * 5 for b in range(5)]
 
 
 def shape_board(board):
-    pass
+    """
+    Restuctures list sequence to a game board style.
+    """
+    rows = 1
+    for row in board:
+        print(rows, '|'.join(row))
+        rows += 1
 
 
 def make_ships(board):
-    pass
-
+    """
+    Creates five ships in random locations.
+    """
+    for ship in range(5):
+        ship_row = randint(0, 4)
+        ship_column = randint(0, 4)
+        while board[ship_row][ship_column] != 'X':
+            board[ship_row][ship_column] = 'X'
+            
 
 def pick_a_ship():
     pass
