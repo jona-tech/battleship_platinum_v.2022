a5cddbdb6bd20b89d208098b5fd0a8df99b32790
jona-tech <jonathanbernhardsson1@gmail.com>
2022-12-12 11:34:43 +0000

    initial commit,add  basic variables and function structures

--- run.py
+++ run.py
@@ -1,3 +1,21 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+
+# Import function to generate random integers
+from random import randint
+
+
+# Declaring sequences for the game board.
+INVISIBLE_BOARD = [[' '] * 5 for b in range(5)] 
+VISIBLE_BOARD = [[' '] * 5 for b in range(5)]
+
+
+def shape_board(board):
+    pass
+
+
+def make_ships(board):
+    pass
+
+
+def pick_a_ship():
+    pass
